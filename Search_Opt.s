3d983fecbd6970934403d4a4c954603b830f5e73
AlbatraozRUS <55453060+AlbatraozRUS@users.noreply.github.com>
2020-05-12 18:46:01 +0300

    Optimized search

--- Search_Opt.s
+++ Search_Opt.s
@@ -4,41 +4,33 @@ extern strcmp
 section .text
 
 SearchByValue:
-       push r13
-       push r12
        push r15
        push rbx
-       mov     rcx, 1000
-       mov     r13, rdi
-  .Around:
-         mov     r12, QWORD  [r13+8]
-         mov     r15, QWORD  [r13]
-         xor     ebx, ebx
+       mov  QWORD r15, [rdi]
+       xor     ebx, ebx
       .Inside:
-             cmp     rbx, r12
-             ja     .AroundNext
+             cmp     rbx, rdx
+             ja     .Not_Found
              mov     rdi, QWORD  [r15]
              test    rdi, rdi
              je      .InsideNext
              call    strcmp
              test    eax, eax
              je      .Found
+
       .InsideNext:
              inc     rbx
              add     r15, 16
              jmp .Inside
-  .AroundNext:
-         add     r13, 40
-         dec     rcx
-         test    ecx, ecx
-         jne     .Around
+  .Not_Found:
          xor     rax, rax
 .EXX:
          pop rbx
          pop r15
-         pop r12
-         pop r13
          ret
+         
 .Found:
        mov     rax, 1
-       jmp .EXX
+       pop rbx
+       pop r15
+       ret
